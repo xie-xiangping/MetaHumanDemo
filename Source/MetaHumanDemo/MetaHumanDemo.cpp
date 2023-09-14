8ee186b0ea2282f4d3175cd2accf1057854d69e2
xie-xiangping <guetNapo@163.com>
2023-09-14 14:26:29 +0800

    first commit

---
+++ Source/MetaHumanDemo/MetaHumanDemo.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MetaHumanDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MetaHumanDemo, "MetaHumanDemo" );
---
+++ Source/MetaHumanDemo/MetaHumanDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MetaHumanDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class METAHUMANDEMO_API AMetaHumanDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
